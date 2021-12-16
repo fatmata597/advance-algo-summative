9c70eea9e846e0eddbc808e086a3f884b78f7bb7
fatmata597 <55181515+fatmata597@users.noreply.github.com>
2021-12-16 15:04:39 +0200

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,6 +1,9 @@
 def firstnsum(n):
-    x = 0
+    count = 0
     for i in range(1,n+1):
-        x+=i
-    return x
-print(firstnsum(1000000000))
+        count+=i
+    return count
+print(firstnsum(10))
+#print(firstnsum(10000))
+#print(firstnsum(1000000))
+#print(firstnsum(1000000000))
